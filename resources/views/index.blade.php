de6d1d44f2162776a7724b8b18621fbc2951110d
minzacm <minzacn@gmail.com>
2021-03-17 14:42:24 +0530

    msg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Employee</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -50,7 +50,7 @@
 </tr>
 <tr>
     <td>DOB</td>
-    <td><date class="form-control"></date></td>
+    <td><input type="date" class="form-control"></date></td>
 </tr>
 <tr>
     <td>Place</td>
